83233090e86f89aeb7d61a970022b4b1c6ba6a80
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-10-19 10:30:42 -0600

    added initial state and reducer function in reducer file

---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,14 @@
+const InitialState = {
+  todoList: [
+    { item: "Learn about reducers", completed: false, id: 3892987589 },
+    { item: "Understand switch", completed: false, id: 3892987588 },
+    { item: "understand dispatch", completed: false, id: 3892987587 },
+    { item: "Learn about redux", completed: false, id: 3892987586 },
+  ]
+};
+
+const Reduce = () => {
+  return <></>;
+};
+export default Reduce;
+export default InitialState;
